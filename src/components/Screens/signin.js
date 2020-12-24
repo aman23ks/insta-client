d6260a34f83433844a06cd4eb5aa03822cf4626b
Aman Shrivastava <nks.ns84@gmail.com>
2020-12-24 21:51:57 +0530

    create post

--- src/components/Screens/signin.js
+++ src/components/Screens/signin.js
@@ -1,17 +1,69 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
-const signin = () => {
+const SignIn = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const PostData = () => {
+    if (
+      !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        email
+      )
+    ) {
+      M.toast({ html: "Invalid Email", classes: "#c62828 red darken-3" });
+      return;
+    }
+
+    fetch("/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
+        } else {
+          M.toast({
+            html: "Signed In Successfully",
+            classes: "#43a047 green darken-1",
+          });
+          history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="mycard">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button
           className="btn waves-effect waves-light #64b5f6 blue darken-1"
-          type="submit"
-          name="action"
+          onClick={() => PostData()}
         >
           Login
         </button>
@@ -23,4 +75,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default SignIn;
